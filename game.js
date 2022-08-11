f2cd402add8ff9136e6c2be18a4a1549d3991db0
Anton <anton20020426@gmail.com>
2022-08-11 13:33:39 +0200

    Make input limited to only 3 options+comment steps

--- game.js
+++ game.js
@@ -1,23 +1,21 @@
+// 1. create a random choice mechanism for computer
+
+const gameArray = ['rock', 'paper', 'scissors']; 
 
 function getComputerChoice() {
-    const gameArray = ['rock', 'paper', 'scissors'];
-    let randomItem = gameArray[Math.floor(Math.random() * 3)];
+    let randomItem = gameArray[Math.floor(Math.random() * 3)]; //picks one option among 0 1 2; works wit strings as well as with numbers
     return randomItem;
 }
 
-// limit input to the three options (no number, no empty string) otherwise alert an error. If there's the alert, the game function mustn't run!
+// 2. create a function for one round, it's like a sample for the 5-round function
 
-// function dontStartGame
-
-// input !== NaN ? alert('Choose these: paper, scissors, rock') : '';
-
-// else if (computerSelection == 'rock' && playerSelection == 'paper' || computerSelection == 'scissors' && playerSelection == 'rock' || computerSelection == 'paper' && playerSelection == 'scissors')
+let input; //declare this variable in a global scope so that each new round will ask for input
 
 function playRound(playerSelection, computerSelection) {
 
-    let input = prompt('Your choice?', '');
+    input = prompt('Your choice?', '');
     playerSelection = input;
-    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase(); //non-sensitive input, any correct option (PaPer/sciSSorS/ROCK will be converted to lower case)
 
     if (computerSelection == playerSelection) {
         return 'Tie';
@@ -27,10 +25,16 @@ function playRound(playerSelection, computerSelection) {
         return 'You lose! Rock beats scissors';
     } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
         return 'You lose! Scissors beat paper';
-    } else {
+    } else if (
+        computerSelection == 'rock' && playerSelection == 'paper' || computerSelection == 'scissors' && playerSelection == 'rock' || computerSelection == 'paper' && playerSelection == 'scissors'
+    ) {
         return 'You won';
+    } else { 
+        return 'error'; //deals with empty strings, numbers and other random values that are not game choices  
     }
 }
+
+// 3. create the 5-round function that keeps the score of rounds and announces a winner after 5 rounds 
 
     let player = 0;
     let computer = 0;
@@ -39,20 +43,24 @@ function playRound(playerSelection, computerSelection) {
 function game() {
 
     for (let i = 0; i < 5; i++){
-        let result = playRound('rock', getComputerChoice());
+        let result = playRound(input, getComputerChoice()); // add two arguments to where function parameters are
 
         if (result == 'You won') {
-            ++player;
-            console.log(`Player score: ${player}`);
+            ++player; //incrementing by 1 the score of player
+            console.log(`Player won: ${player} - ${computer}`); //keep the score updated in real time
         } else if (result == 'You lose! Paper beats Rock' || result == 'You lose! Rock beats scissors' || result == 'You lose! Scissors beat paper'){
             ++computer;
-            console.log(`Computer score: ${computer}`)
+            console.log(`Computer won: ${player} - ${computer}`)
+        } else if (result == 'Tie'){
+            ++player;
+            ++computer;
+            console.log(`Tie: ${player} - ${computer}`);
         } else {
-            console.log('tie');
+            alert('Choose one of these: rock, paper, or scissors'); 
         }
     }
 
-    switch(true) {
+    switch(true) { //determining a winner
         case player>computer:
             winner = 'Winner: You';
             break;
@@ -61,9 +69,13 @@ function game() {
             winner = 'Winner: Computer';
             break;
             
-        default: winner = "Winner: Neither, it's a freaking tie xD";
-    }
-    return winner;
+        case computer === player && computer !==0 && player !==0:
+            winner = "Winner: Neither, it's a freaking tie xD";
+            break;
+
+        default: winner = 'Nothing';
+        }
+        return winner; //returning this value so that it's stored in the global scope variable called "winner"
 }
 
 console.log(game());
