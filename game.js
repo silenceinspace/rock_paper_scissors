f6a5dde7b34b61a82ae8cc2ace3a1f56856a71a0
Anton <anton20020426@gmail.com>
2022-09-08 12:15:48 +0200

    Finish the game with all functionality

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ function getComputerChoice() {
     return randomItem;
 }
 
-// 2. create a function for one round
+// 2. use buttons for getting values
 const buttons = document.querySelectorAll('button');
 let playerChoice = buttons.forEach((btn) => {
     btn.addEventListener('click', e => {
@@ -21,21 +21,55 @@ let playerChoice = buttons.forEach((btn) => {
     });
 });
 
-const div = document.querySelector('.outcome'); 
+// 3. set rules for game
 function playRound(playerSelection, computerSelection) {
 
     if (computerSelection == playerSelection) {
-        div.textContent = 'Tie'; //use return instead 
+        div.textContent = 'Tie';
+        score.textContent = `Tie: ${player} - ${computer}`; 
     } else if (computerSelection == 'paper' && playerSelection == 'rock') {
         div.textContent = 'You lose! Paper beats Rock';
+        lose();
     } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
         div.textContent = 'You lose! Rock beats scissors';
+        lose();
     } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
         div.textContent = 'You lose! Scissors beat paper';
+        lose();
     } else if (
         computerSelection == 'rock' && playerSelection == 'paper' || computerSelection == 'scissors' && playerSelection == 'rock' || computerSelection == 'paper' && playerSelection == 'scissors'
     ) {
         div.textContent = 'You won';
-    }
+        ++player;
+        score.textContent = `Player won: ${player} - ${computer}`; 
+    } return gameOver();
 }
 
+function lose(){
+    ++computer;
+    score.textContent = `Computer won: ${player} - ${computer}`;
+};
+
+// 4. track and display progress of game
+const div = document.querySelector('.outcome');
+const score = document.querySelector('.score');
+let player = 0;
+let computer = 0;
+let winner;
+
+let hide = document.getElementById('container');
+
+function gameOver() {
+    switch(true) {
+        case player === 5 && player>computer:
+            div.textContent = 'Winner: You';
+            hide.style.display = 'none';
+            break;
+            
+        case computer === 5 && computer>player:
+            div.textContent = 'Winner: Computer';
+            hide.style.display = 'none';
+            break;
+    } 
+    return winner = div.textContent;
+}
